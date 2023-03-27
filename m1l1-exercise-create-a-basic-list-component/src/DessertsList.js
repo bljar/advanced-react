23f9c6a66464641b96ef6c4f910310f48e1add91
Belajar <bljkmp@gmail.com>
2023-03-27 09:25:50 +0700

    implement desertList component

--- m1l1-exercise-create-a-basic-list-component/src/DessertsList.js
+++ m1l1-exercise-create-a-basic-list-component/src/DessertsList.js
@@ -1,6 +1,16 @@
 function DessertsList(props) {
   // Implement the component here.
-  return null;
+  return (
+    <div>
+      <ul>{ props.data
+        .filter(desert => desert.calories < 500)
+        .sort((a, b) => a.calories - b.calories)
+        .map(filteredDesert => (
+          <li>{filteredDesert.name} - {filteredDesert.calories} cal.</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default DessertsList;
