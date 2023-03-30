4097681fb11afb168f736eeb1c06c7da84eb4a5d
Belajar <bljkmp@gmail.com>
2023-03-30 09:52:07 +0700

    Tweak the implementation of PointMouseLogger

--- m3l2-exercise-implementing-scroller-positon-with-render-props/src/App.js
+++ m3l2-exercise-implementing-scroller-positon-with-render-props/src/App.js
@@ -46,15 +46,16 @@ const PanelMouseLogger = () => {
 };
 
 // This component should not receive any props
-const PointMouseLogger = ({mousePosition}) => {
+const PointMouseLogger = () => {
   // The below if statement can be removed after the render props pattern is implemented
-  if (!mousePosition) {
-    return null;
-  }
   return (
-    <p>
-      ({mousePosition.x}, {mousePosition.y})
-    </p>
+    <MousePosition 
+      render={({mousePosition}) => (
+        <p>
+          ({mousePosition.x}, {mousePosition.y})
+        </p>
+      )}
+    />
   )
 };
 
