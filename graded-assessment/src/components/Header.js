a02e8f0ae3b7f90ffb7b429bf3ab6547471ba5f7
Belajar <bljkmp@gmail.com>
2023-04-01 14:29:52 +0700

    hide navigation bar when scroll down

--- graded-assessment/src/components/Header.js
+++ graded-assessment/src/components/Header.js
@@ -33,6 +33,28 @@ const socials = [
 ];
 
 const Header = () => {
+  const headerRef = useRef(null);
+  
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+    
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const headerElement = headerRef.current;
+
+      if (headerElement && prevScrollPos > currentScrollPos) {
+        headerElement.style.transform = "translateY(0)";
+      } else if (headerElement) {
+        headerElement.style.transform = "translateY(-200px)";
+      }
+      
+      prevScrollPos = currentScrollPos;
+    }
+    
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -55,6 +77,7 @@ const Header = () => {
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
       backgroundColor="#18181b"
+      ref={headerRef}
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
@@ -65,11 +88,11 @@ const Header = () => {
         >
           <nav>
             <HStack spacing={8}>
-              {socials.map(s =>
-                <a href={s.url}>
-                  <FontAwesomeIcon icon={s.icon} size="2x" />
-                </a>
-              )}
+              {socials.map(({ icon, url }) => ( 
+                <a href={url} > 
+                  <FontAwesomeIcon icon={icon} size="2x" key={url} /> 
+               </a> 
+             ))}
             </HStack>
           </nav>
           <nav>
