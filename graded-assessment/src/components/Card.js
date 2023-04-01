c612d55d2718eeac50f6eca6472ea7ec7c5a30f9
Belajar <bljkmp@gmail.com>
2023-04-01 07:57:17 +0700

    implement card component

--- graded-assessment/src/components/Card.js
+++ graded-assessment/src/components/Card.js
@@ -7,7 +7,24 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <HStack>
+      <VStack
+        align="initial"
+        color="black"
+        background="white"
+        borderRadius="lg"
+      >
+        <Image src={imageSrc} alt={title} borderRadius="lg"/>
+        <Heading padding={2} size="md">{title}</Heading>
+        <Text padding={2} color="gray.600">{description}</Text>
+        <HStack padding={2}>
+          <Text as='b'>See more</Text>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </HStack>
+  );
 };
 
 export default Card;
